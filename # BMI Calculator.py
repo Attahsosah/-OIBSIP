0917fdf0089f802cda20a2e13061a163ce710b3b
Attahsosah <sosahattah55@gmail.com>
2024-09-17 17:49:54 +0200

    UX updates

--- # BMI Calculator.py
+++ # BMI Calculator.py
@@ -1,57 +1,75 @@
 # BMI Calculator
-import tkinter as tk
+import tkinter as kinter
 from tkinter import messagebox
 # Formula  for BMI: Body weight(kg) / height(m**2) 
 
 # BMI Index : Below 18.5 = underweight , 18.5 - 24.9  = Healthy range, 25.0 - 29.9 = Overweight, >/=30.0  = Obese
 def bmi_calculator():
-    underWeight = 18.5 
-    # healthyRange = range (float(18.5), float(24.9))
-    # overWeight  = range(25.0, 29.9 )
-    weight =  float(weight_in.get())
-    
-    height =  float(height_in.get())
-    BMI = weight / (height**2)
-    
-    BMI_label.config(text = f"Your BMI is: {BMI:.2f}")
     
     
-    if BMI < underWeight:
-        result.config(text="According to your BMI, you are underweight")
+    
+  try:
+        underWeight = 18.5 
         
-    elif 18.5 <= BMI <= 24.9 :
-        result.config(text="You are within normal weight parameters")
-    
-    elif 25.0 <= BMI <= 29.9 :
-        result.config(text="You are overweight")
+        weight = float(weight_spinbox.get())
         
-    else: 
-        result.config(text="According to your BMI, you are obese")
+        height = float(height_spinbox.get())
+        
+        BMI = weight / (height**2)
+        
+        BMI_label.config(text = f"Your BMI is: {BMI:.2f}")
+        
+        
+        if BMI < underWeight:
+            result.config(text="According to your BMI, you are underweight")
+            
+        elif 18.5 <= BMI <= 24.9 :
+            result.config(text="You are within normal weight parameters")
+        
+        elif 25.0 <= BMI <= 29.9 :
+            result.config(text="You are overweight")
+            
+        else: 
+            result.config(text="According to your BMI, you are obese")
+  except:
+      messagebox.showerror('Sorry the answer:', result,'is not valid, please try using numbers')
+      
+      overweight_color = "#FF9800"
+      normalweight_color = "#4CAF50"
+      
         
 #   set_default_color_theme('blue')      
 # Main window tkinter
-root = tk.Tk()
-root.title=("BMI Calculator")
+root = kinter.Tk()
+root.title("The BMI Calculator")
 
-root.configure(bg='#00008B')
+root.configure(bg='#428df5')
 
 # The widget
-tk.Label(root, text="Weight (In Kg):", bg='#00008B', fg='white').grid(row=0, column=0)
-weight_in = tk.Entry(root)
-weight_in.grid(row=0, column=1)
+kinter.Label(root, text="Please fill in the following information").grid(row=0, column=0)
+kinter.Label(root, text=" Below 18.5 = underweight").grid(row=1, column=0)
+kinter.Label(root, text=" 18.5 - 24.9  = Healthy range",bg="green").grid(row=2, column=0)
+kinter.Label(root, text=" 25.0 - 29.9 = Overweight", bg="orange").grid(row=3, column=0)
 
-tk.Label(root, text="Height (In Metres):",  bg='#00008B', fg='white').grid(row=1, column=0)
-height_in = tk.Entry(root)
-height_in.grid(row=1, column=1)
 
-submit = tk.Button(root, text="Calculate", command=bmi_calculator, bg='red', fg='red')
-submit.grid(row=2, columnspan=2)
 
-BMI_label = tk.Label(root, text="Your BMI is:" , bg='#00008B')
-BMI_label.grid(row=3, columnspan=2)
 
-result = tk.Label(root, text="", bg='#00008B')
-result.grid(row=4, columnspan=2)
+kinter.Label(root, text="Weight (In Kg):", bg='#428df5', fg='white').grid(row=1, column=1)
+weight_spinbox = kinter.Spinbox(root, from_=30.0, to=300.0, increment=1, width=10, bg="black")
+weight_spinbox.grid(row=1, column=2)
+
+kinter.Label(root, text="Height (In Metres):",  bg='#428df5', fg='white').grid(row=2, column=1)
+height_spinbox = kinter.Spinbox(root, from_=1.0, to=2.5, increment=0.01, width=10, bg="black")
+height_spinbox.grid(row=2, column=2)
+
+submit = kinter.Button(root, text="Calculate", command=bmi_calculator, bg='red', fg='black')
+submit.grid(row=5, columnspan=5)
+
+BMI_label = kinter.Label(root, text="Your BMI is:" , bg='#428df5')
+BMI_label.grid(row=4, columnspan=3)
+
+result = kinter.Label(root, text="", bg='#428df5')
+result.grid(row=7, columnspan=3)
 
 root.mainloop()
 
