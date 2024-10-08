0b8f2ac92193c1e8b4e22be21ee388661f1a27e9
Attahsosah <sosahattah55@gmail.com>
2024-10-08 09:50:05 +0200

    changes to structure

---
+++ BMICalc/# BMI Calculator.py
@@ -0,0 +1,81 @@
+# BMI Calculator
+import tkinter as kinter
+from tkinter import messagebox
+# Formula  for BMI: Body weight(kg) / height(m**2) 
+
+# BMI Index : Below 18.5 = underweight , 18.5 - 24.9  = Healthy range, 25.0 - 29.9 = Overweight, >/=30.0  = Obese
+def bmi_calculator():
+    
+      
+  try:
+      
+        underWeight = 18.5 
+        # spinbox is simply a number toggler
+        weight = float(weight_spinbox.get())
+        
+        height = float(height_spinbox.get())
+        
+        BMI = weight / (height**2)
+        
+        BMI_label.config(text = f"Your BMI is: {BMI:.2f}")
+        
+        
+        if BMI < underWeight:
+            result.config(text="According to your BMI, you are underweight")
+            
+        elif 18.5 <= BMI <= 24.9 :
+            result.config(text="You are within normal weight parameters", bg="green")
+        
+        elif 25.0 <= BMI <= 29.9 :
+            result.config(text="You are overweight", bg="orange")
+            
+        else: 
+            result.config(text="According to your BMI, you are obese", bg="red")
+  except:
+      messagebox.showerror('Sorry the answer:', result,'is not valid, please try using numbers')
+      
+     
+      
+        
+#   set_default_color_theme('blue')      
+# Main window tkinter
+root = kinter.Tk()
+root.title("The BMI Calculator")
+
+root.configure(bg='#96928d')
+
+# The widget
+kinter.Label(root, text="Please fill in the following information").grid(row=0, column=0)
+kinter.Label(root, text=" Below 18.5 = underweight").grid(row=1, column=0)
+kinter.Label(root, text=" 18.5 - 24.9  = Healthy range",bg="green").grid(row=2, column=0)
+kinter.Label(root, text=" 25.0 - 29.9 = Overweight", bg="orange").grid(row=3, column=0)
+
+
+
+
+kinter.Label(root, text="Weight (In Kg):", bg='#96928d', fg='white').grid(row=1, column=1)
+weight_spinbox = kinter.Spinbox(root, from_=30.0, to=300.0, increment=1, width=10, bg="black")
+weight_spinbox.grid(row=1, column=2)
+
+kinter.Label(root, text="Height (In Metres):",  bg='#96928d', fg='white').grid(row=2, column=1)
+height_spinbox = kinter.Spinbox(root, from_=1.0, to=2.5, increment=0.01, width=10, bg="black")
+height_spinbox.grid(row=2, column=2)
+
+submit = kinter.Button(root, text="Calculate", command=bmi_calculator, bg='red')
+submit.grid(row=5, columnspan=5)
+
+BMI_label = kinter.Label(root, text="Your BMI is:" , bg='#96928d')
+BMI_label.grid(row=4, columnspan=3)
+
+result = kinter.Label(root, text="", bg='#96928d')
+result.grid(row=7, columnspan=3)
+
+root.mainloop()
+
+
+    
+    
+    
+
+
+
